e94a22862074b05bbea5a3c4ad82e166a88f558c
bebusl <bebus1998@naver.com>
2022-10-05 00:08:56 +0900

    chore: fix broken files

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { NextPageContext } from "next";
+import Image from "next/image";
+import React, { useRef } from "react";
+import Button from "../../components/common/Button";
+import Input from "../../components/common/Input";
+import AuthTemplate from "../../components/templates/AuthTemplate";
 
-function register() {
-  return <div>register</div>;
+function Register({ provider }: { provider: string | undefined }) {
+  const nicknameRef = useRef<HTMLInputElement>(null);
+  const idRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  return (
+    <AuthTemplate
+      title="REGISTER"
+      subTitle="서비스를 이용하기 위해서는 회원가입이 필요합니다"
+    >
+      {provider ? (
+        <>
+          <Image
+            src={`/images/logo/${provider.toLowerCase()}-logo.png`}
+            alt={`login with ${provider.toLowerCase()}`}
+            height="64px"
+            width="64px"
+          />
+          <p>{provider}와(과) 연동되는 계정입니다.</p>
+        </>
+      ) : (
+        <p>연동된 소셜 계정이 없습니다</p>
+      )}
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <Input name="nickname" ref={nicknameRef} />
+        <Input name="id" ref={idRef} />
+        <Input name="password" ref={passwordRef} />
+        <Button type="submit">SIGNUP</Button>
+      </form>
+    </AuthTemplate>
+  );
 }
 
-export default register;
+export function getServerSideProps(ctx: NextPageContext) {
+  const provider = ctx.query?.provider ?? null;
+
+  return {
+    props: {
+      provider,
+    },
+  };
+}
+
+export default Register;
