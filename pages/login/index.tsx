211dfbf7588da7179e37310e108883e1857b8196
bebusl <bebus1998@naver.com>
2022-10-23 15:29:22 +0900

    feat: 로그인,회원가입 커스텀훅을 통해 기능 구현

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -11,6 +11,7 @@ import {
   GOOGLE_AUTH_URL,
   KAKAO_AUTH_URL,
 } from "../../constants/url";
+import useForm from "../../hooks/useForm";
 
 const OAuthLinkButton = React.memo(
   ({ url, type }: { url: string; type: "kakao" | "github" | "google" }) => {
@@ -29,84 +30,47 @@ const OAuthLinkButton = React.memo(
 );
 
 const LoginForm = () => {
-  const idRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
   const dispatch = useAuthDispatch();
   const router = useRouter();
-
-  const passwordValidator = (value: string) => {
-    const passwordRegex =
-      /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
-    const result = passwordRegex.test(value);
-    console.log("RESULT ::: ", result);
-    return result;
-  };
-
-  const emailValidator = (value: string) => {
-    const emailRegex =
-      /[a-zA-Z0-9]+([-_\.][a-zA-Z0-9]*)*@[a-zA-Z]{3,}\.[a-z]{2,4}/;
-    const result = emailRegex.test(value);
-    return result;
-  };
-
-  const [isDirtyField, setDirtyField] = useState({
-    id: false,
-    password: false,
+  const { handleBlur, isValidInputs, isValid, getRef } = useForm({
+    types: ["email", "password"],
   });
-  const [isValid, setIsValid] = useState({ id: true, password: true });
-
-  const isValidInputs = () => {
-    const a = Object.values(isDirtyField).every((field) => field);
-    const b = Object.values(isValid).every((field) => field);
-    return a && b;
-  };
+  const emailRef = getRef("email");
+  const passwordRef = getRef("password");
 
   const checkData = async () => {
-    const id = idRef.current?.value;
-    const password = passwordRef.current?.value;
-    if (id && password) {
-      dispatch(loginRequest({ id, password }))
-        .unwrap()
-        .then(() => router.push("/"))
-        .catch((e) => console.log("ERROR", e));
-    }
+    const id = emailRef.current?.value || "";
+    const password = passwordRef.current?.value || "";
+    dispatch(loginRequest({ id, password }))
+      .unwrap()
+      .then(() => router.push("/"))
+      .catch((e) => console.log("ERROR", e));
   };
   return (
-    <>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        checkData();
+      }}
+    >
       <OAuthLinkButton url={GOOGLE_AUTH_URL} type="google" />
       <OAuthLinkButton url={KAKAO_AUTH_URL} type="kakao" />
       <OAuthLinkButton url={GITHUB_AUTH_URL} type="github" />
       <Input
-        name="id"
-        ref={idRef}
-        validator={emailValidator}
-        isValid={isValid.id}
-        onBlur={(value) => {
-          setIsValid({ ...isValid, id: value as boolean });
-          setDirtyField((prev) => ({
-            ...prev,
-            id: "" !== idRef.current?.value,
-          }));
-        }}
+        name="email"
+        ref={emailRef}
+        isValid={isValid.email}
+        onBlur={() => handleBlur("email")}
       />
       <Input
         name="password"
         type="password"
         ref={passwordRef}
         isValid={isValid.password}
-        validator={passwordValidator}
-        onBlur={(value) => {
-          setIsValid({ ...isValid, password: value as boolean });
-          setDirtyField((prev) => ({
-            ...prev,
-            password: "" !== passwordRef.current?.value,
-          }));
-        }}
+        onBlur={() => handleBlur("password")}
       />
-      <Button disabled={!isValidInputs()} onClick={checkData}>
-        LOGIN
-      </Button>
-    </>
+      <Button disabled={!isValidInputs()}>LOGIN</Button>
+    </form>
   );
 };
 
