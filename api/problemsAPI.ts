81cfa43e8c24316c06239d418b4e008d9aa4a88f
bebusl <bebus1998@naver.com>
2022-11-05 14:51:32 +0900

    feat: 문제 가져오는 API props 유동적으로 넣을 수 있게 수정

--- api/problemsAPI.ts
+++ api/problemsAPI.ts
@@ -1,13 +1,33 @@
 import request from "./request";
+import { LANGUAGES } from "../constants/language";
 
 const { get, post, deleteRequest, putRequest } = request("/problems");
+
+interface problemProps {
+  page: string;
+  title?: string;
+  languages?: LANGUAGES[];
+  tags?: number[];
+}
 
 export const deleteProblem = async (id: string) =>
   await deleteRequest(`/${id}`);
 
 export const getProblemDetail = async (id: string) => await get(`/${id}`);
 
-export const getProblems = async () => await get("");
+export const getProblems = async ({
+  page,
+  title,
+  languages = [],
+  tags,
+}: problemProps) => {
+  const defaultQuery = "?page=" + page;
+  const titleQuery = title ? `&title=${title}` : "";
+  const languagesQuery =
+    languages?.length > 0 ? `&languages=${languages[0]}` : "";
+
+  return await get(defaultQuery + titleQuery + languagesQuery);
+};
 
 export const registerProblem = async () => await post({ url: "" });
 
