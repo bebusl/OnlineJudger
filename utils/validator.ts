396576bcaadf55859d651d7904df9de48fd7b8ee
bebusl <bebus1998@naver.com>
2022-11-06 15:13:43 +0900

    feat: Input에 사용되는 password validator 정규식 변경
    * 기존 비밀번호 정규식은 영문자,숫자,특수문자 세 가지를 다 포함하는 8~15자리의 값에만 유효했음
    * 관리자 비밀번호가 해당 조건에 부합하지 않아, 숫자+영문 조합의 6~10자리 비밀번호도 유효하도록 정규식 변경함

--- utils/validator.ts
+++ utils/validator.ts
@@ -1,9 +1,9 @@
-export type regexType = "email" | "password" | "id";
+export type regexType = "email" | "password" | "id" | string;
 
 const regexPatterns: Record<regexType, RegExp> = {
   email: new RegExp("[a-zA-Z0-9]+([-_.][a-zA-Z0-9]*)*@[a-zA-Z]{3,}.[a-z]{2,4}"),
   password: new RegExp(
-    "^.*(?=^.{8,15}$)(?=.*d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$"
+    "(^.*(?=^.{8,15}$)(?=.*d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$)|[a-zA-Z0-9]{6,10}"
   ),
   id: new RegExp("[가-힣a-zA-Z0-9]{6,12}"),
 };
