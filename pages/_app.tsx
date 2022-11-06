c6dbdfffff3cdc8a4c6a10017a770f438475e89c
bebusl <bebus1998@naver.com>
2022-11-06 15:53:04 +0900

    feat: 페이지 별 레이아웃 적용 가능하도록 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,53 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import { useEffect } from "react";
 import { Provider } from "react-redux";
+import store from "../store/store";
+import AuthGuard from "../components/guard/AuthGuard";
 import { ThemeProvider } from "styled-components";
 import theme from "../styles/theme";
 import DefaultLayout from "../components/layouts/DefaultLayout";
-import store from "../store/store";
-import AuthGuard from "../components/guard/AuthGuard";
-import { useEffect } from "react";
 import { getUserData } from "../store/slice/authSlice";
 import Notification from "../components/common/Notification";
 
-function MyApp({ Component, pageProps }: AppProps<{ authRequired: boolean }>) {
-  const requiredAuth = Component.defaultProps?.authRequired;
+import type { ReactElement, ReactNode } from "react";
+import type { AppProps } from "next/app";
+import type { NextPage } from "next";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout<T> = AppProps<T> & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({
+  Component,
+  pageProps,
+}: AppPropsWithLayout<{ authRequired: boolean }>) {
   useEffect(() => {
     store.dispatch(getUserData());
   }, []);
 
+  const getLayout =
+    Component.getLayout || ((page) => <DefaultLayout page={page} />);
+
+  const RequiredAuth = () =>
+    Component.defaultProps?.authRequired ? (
+      <AuthGuard>
+        <Component {...pageProps} />
+      </AuthGuard>
+    ) : (
+      <Component {...pageProps}></Component>
+    );
+
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <DefaultLayout>
-          <Notification />
-          {requiredAuth ? (
-            <AuthGuard>
-              <Component {...pageProps} />
-            </AuthGuard>
-          ) : (
-            <Component {...pageProps} />
-          )}
-        </DefaultLayout>
+        <Notification />
+        {getLayout(<RequiredAuth />)}
       </ThemeProvider>
+      <ThemeProvider theme={theme}></ThemeProvider>
     </Provider>
   );
 }
