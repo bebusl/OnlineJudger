c4d6237526f1592acfd6d45ba630e5fc093cadba
bebusl <bebus1998@naver.com>
2022-11-09 10:26:02 +0900

    feat: 제출이력 확인 api 호출 시 쿼리 다양하게 요청할 수 있도록 수정

--- api/submissionAPI.ts
+++ api/submissionAPI.ts
@@ -14,8 +14,24 @@ export const getAllSubmissions = async (page: number, language?: string) => {
 //모든 제출 기록 가져오기(scoreboard), scoreboard는 모든 페이지 정보 다 들고와서 해야할 것 같음..!
 //맨 첫번째 걸 먼저 들고옴. 1페이지 응답 => total_page저장되어 있음. => Promise.all안에 2페이지부터 total_page까지 결과를 다 가져옴 => data합쳐서 return
 
-export const getSubmissionsByUserId = async (userId: string) =>
-  await get(`?user_id=${userId}`);
+export const getSubmissionsByQuery = async ({
+  userId,
+  problemId,
+  language,
+  page = 0,
+}: {
+  userId?: string;
+  problemId?: number;
+  language?: string;
+  page?: number;
+}) => {
+  const pageQuery = "?page=" + page;
+  const userIdQuery = userId ? "&user_id=" + userId : "";
+  const problemIdQuery = problemId ? "&problem_id=" + problemId : "";
+  const languageQuery = language ? "&language=" + language : "";
+  const query = pageQuery + userIdQuery + problemIdQuery + languageQuery;
+  return await get(query);
+};
 
 //   page: 0,
 //   problem_id: 1,
