bb67ae768a7aa6c3c40379a7844a1c3bb804cc65
bebusl <bebus1998@naver.com>
2022-11-09 10:28:34 +0900

    feat: 마이페이지 - 구글 계정과 연동 기능 추가

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import type { InferGetServerSidePropsType, NextPageContext } from "next/types";
 import React from "react";
 import { getUser } from "../../api/authAPI";
-import { Button, Input } from "../../components/common";
+import { Button } from "../../components/common";
 import { LabelInput } from "../../components/common/Input";
 import WithSideBar from "../../components/templates/WithSideBar";
 import useForm from "../../hooks/useForm";
@@ -15,7 +15,7 @@ function UserProfile({
   avatar_url,
   links,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const { handleBlur, isValidInputs, isValid, getRef } = useForm({
+  const { getRef } = useForm({
     types: ["name", "email", "link"],
   });
   const nameRef = getRef("name");
@@ -33,8 +33,12 @@ function UserProfile({
             width="100px"
             height="100px"
           />
-          <Button>Change</Button>
-          <Button>Remove</Button>
+          <Button $variant="outline" width="fit-content">
+            Change
+          </Button>
+          <Button $variant="outline" width="fit-content">
+            Remove
+          </Button>
         </form>
         <LabelInput
           type="text"
@@ -48,19 +52,33 @@ function UserProfile({
           grandchildRef={emailRef}
           defaultValue={name}
         />
-        <LabelInput grandchildRef={linkRef} type="email" name="linkedEmail" />
-        <a href="https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile&response_type=token&redirect_uri=https://localhost:3443/oauth2/redirect/client&client_id=120623111530-5cf3mkdcfed1m9n8eumn9vsa6dodl43l.apps.googleusercontent.com">
-          구글 연동하기
-        </a>
-        <article>
-          <h3>Email Notification</h3>
-          <Input type="checkbox" isValid={true} name="new deal" />
-          <label htmlFor="new deal">new deal</label>
-        </article>
-        <div>
-          <Button>로그아웃</Button>
-          <Button>초기화</Button>
-          <Button>저장</Button>
+        <h5>연동된 소셜 계정</h5>
+
+        {links.length > 0 ? (
+          <LabelInput
+            grandchildRef={linkRef}
+            type="email"
+            name="linkedEmail"
+            value={links[0]}
+          />
+        ) : (
+          <>
+            <p>연동된 소셜 계정이 없습니다</p>
+            <a href="https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile&response_type=token&redirect_uri=https://localhost:3443/oauth2/redirect/client&client_id=120623111530-5cf3mkdcfed1m9n8eumn9vsa6dodl43l.apps.googleusercontent.com">
+              구글 연동하기
+            </a>
+          </>
+        )}
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Button $variant="outline" width="fit-content">
+            로그아웃
+          </Button>
+          <div>
+            <Button $variant="outline" width="fit-content">
+              초기화
+            </Button>
+            <Button width="fit-content">저장</Button>
+          </div>
         </div>
       </section>
     </WithSideBar>
@@ -69,13 +87,15 @@ function UserProfile({
 
 export async function getServerSideProps(ctx: NextPageContext) {
   const auth_cookie = ctx.req?.cookies.Authorization;
-  const userInfo = await getUser(auth_cookie);
+  if (auth_cookie) {
+    const userInfo = await getUser(auth_cookie);
 
-  if (userInfo.data.success) {
-    console.log("여기러 옵니다!");
-    return {
-      props: { ...userInfo.data.user },
-    };
+    if (userInfo.data.success) {
+      console.log("여기러 옵니다!");
+      return {
+        props: { ...userInfo.data.user },
+      };
+    }
   }
   return {
     redirect: {
