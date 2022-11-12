7ff980a0d9138112ef2049fc51c7b28ac36b3c20
bebusl <bebus1998@naver.com>
2022-11-12 12:08:12 +0900

    feat: 제출한 코드 조회 페이지 구현

--- pages/solution/[problemId].tsx
+++ pages/solution/[problemId].tsx
@@ -1,31 +1,50 @@
-import { NextPageContext } from "next";
 import React, { ReactElement } from "react";
-import { getSubmissionsByQuery } from "../../api/submissionAPI";
+import dynamic from "next/dynamic";
+import type { NextPageContext } from "next";
+import { getSubmissionsByQuery } from "../../api/submissionsAPI";
 import Pagination from "../../components/common/Pagination";
+const MonacoEditor = dynamic(import("@monaco-editor/react"), { ssr: false });
 
+interface submissionResponse {
+  user_id: string;
+  problem_id: number;
+  language: string;
+  status: "SUCCESS" | "FALSE" | "PENDING";
+  memory: number;
+  real_time: number;
+  created_at: string;
+  updated_at: string;
+  code_length: number;
+  code: string;
+}
 function SolvePage({
   pageInfo,
   submissions,
+  user,
 }: {
   pageInfo: { [key: string]: number | boolean };
-  submissions: Record<string, unknown>[];
+  submissions: submissionResponse[];
+  user: string;
 }) {
-  //   {
-  //   user_id: 'test1234',
-  //   problem_id: 6,
-  //   language: 'C',
-  //   status: 'SUCCESS',
-  //   memory: 1,
-  //   real_time: 0,
-  //   created_at: '2022-11-05T05:43:07.326',
-  //   updated_at: '2022-11-05T05:43:07.51',
-  //   code_length: 232
-  // },
-  //아 아직 제출한 코드 보여주는 api가 없구나,,!
   return (
     <section>
-      <h1>내가 푼 문제</h1>
-      <p>여기에 editor readonly로 작성한 코드 보여줄것임!</p>
+      <h1>{user}님의 제출 이력</h1>
+      {submissions.length === 0 ? (
+        <p>제출한 코드가 없습니다.</p>
+      ) : (
+        <>
+          {submissions.map((submission) => (
+            <MonacoEditor
+              options={{ readOnly: true }}
+              key={submission.created_at}
+              defaultValue={submission.code}
+              width="80%"
+              height="100vh"
+            />
+          ))}
+        </>
+      )}
+
       <Pagination route="/solution/6?user=test1234" {...pageInfo} />
     </section>
   );
@@ -38,15 +57,19 @@ export async function getServerSideProps(ctx: NextPageContext) {
       userId: user as string,
       problemId: +problemId,
     });
-    console.log("SUBMISSIONS", result.data.submissions);
-    if (result.data.success) {
+
+    if (result.data.success && result.data.page.total_elements > 0) {
       return {
         props: {
+          user: user,
           pageInfo: result.data.page,
           submissions: result.data.submissions,
         },
       };
     }
+    return {
+      notFound: true,
+    };
   } else
     return {
       notFound: true,
