9b271655cc47cc008d4f6e25549017b25b8998bb
bebusl <bebus1998@naver.com>
2022-11-28 23:53:45 +0900

    fix: redux로 인해 발생한 hydrate오류 해결

--- components/layouts/Header.style.ts
+++ components/layouts/Header.style.ts
@@ -23,39 +23,3 @@ export const Logo = styled.span`
   text-align: left;
   color: ${({ theme }) => theme.colors.black};
 `;
-
-export const SearchBarWrapper = styled.form`
-  width: 370px;
-  height: 40px;
-  flex-grow: 0;
-  padding: 10px 12px;
-  border-radius: 12px;
-  background-color: ${({ theme }) => theme.colors.gray100};
-  display: flex;
-
-  & > button {
-    width: 30px;
-    height: 30px;
-
-    object-fit: contain;
-    background-color: inherit;
-    box-shadow: none;
-    border: none;
-  }
-  & > input {
-    background-color: inherit;
-    border: none;
-    width: 310px;
-    height: 20px;
-    margin: 0 20px 0 0;
-    font-family: Nunito;
-    font-size: 14px;
-    font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: 1.43;
-    letter-spacing: 0.1px;
-    text-align: left;
-    color: ${({ theme }) => theme.colors.gray300};
-  }
-`;
