c4e8911d1235577c05a8b6396ad6bb63071efc19
bebusl <bebus1998@naver.com>
2022-12-02 21:15:38 +0900

    feat: 문제 보기 페이지 검색 기능 적용

--- pages/problem/index.tsx
+++ pages/problem/index.tsx
@@ -7,9 +7,9 @@ import { getProblems } from "../../api/problemsAPI";
 import { FlexBox } from "../../components/common";
 import Pagination from "../../components/common/Pagination";
 import Popover from "../../components/common/Popover";
-import SearchBar from "../../components/common/SearchBar";
 import Table from "../../components/common/Table";
 import type { TableProps } from "../../components/common/Table";
+import SearchFilter from "../../components/search/SearchFilter";
 
 interface ProblemProps extends TableProps {
   problems: Record<string, string | number | JSX.Element>[];
@@ -38,93 +38,62 @@ export default function ProblemList({
     setBody(test);
   }, []); // problems를 useEffect안에서 말고 밖에서 조작하니까 hydration에러가 나서 이렇게 처리해줌.
 
-  // const body = problems.map((problem) => ({
-  //   ...problem,
-  //   title: (
-  //     <Link key={problem.title} href={`problem/${problem.id}`}>
-  //       {problem.title}
-  //     </Link>
-  //   ),
-  //   languages: problem.languages?.join(" "),
-  // }));
-  // const body = problems.reduce(
-  //   (
-  //     pre: (string | number | JSX.Element)[][],
-  //     cur: Record<string, string | number | JSX.Element>
-  //   ) => {
-  //     const language_tag = (
-  //       <div
-  //         style={{ width: "100%", height: "100%" }}
-  //         onMouseEnter={(e) => {
-  //           const currentTarget = e.currentTarget;
-  //           const clientRect = currentTarget.getBoundingClientRect();
-  //           setPopOver({ top: clientRect.top, left: clientRect.left });
-  //         }}
-  //         onMouseLeave={(e) => {
-  //           setPopOver(null);
-  //         }}
-  //       >
-  //         {cur.languages.map((language) => (
-  //           <Tag key={language}>{language}</Tag>
-  //         ))}
-  //       </div>
-  //     );
-  //     const data = [
-  //       cur.id,
-  //       <Link href={`problem/${cur.id}`} key={cur.id}>
-  //         {cur.title}
-  //       </Link>,
-  //       cur.memory_limit,
-  //       cur.time_limit,
-  //       language_tag,
-  //     ];
-  //     return [...pre, data];
-  //   },
-  //   []
-  // );
   return (
     <React.Suspense fallback={<Error />}>
-      <SearchBar />
+      <SearchFilter />
 
       {body?.length ? (
-        <Table header={header} body={body} />
+        <>
+          <Table header={header} body={body} />
+          <Pagination
+            route="problem"
+            current_pages={pageInfo.current_pages}
+            total_pages={pageInfo.total_pages}
+          />
+        </>
       ) : (
-        <FlexBox style={{ minHeight: "90vh" }}>등록된 문제가 없습니다.</FlexBox>
+        <FlexBox style={{ minHeight: "80vh" }}>등록된 문제가 없습니다.</FlexBox>
       )}
-      {popoverState && (
-        <Popover top={popoverState.top} left={popoverState.left} />
-      )}
-
-      <Pagination
-        route="problem"
-        current_pages={pageInfo.current_pages}
-        total_pages={pageInfo.total_pages}
-      />
     </React.Suspense>
   );
 }
 
 export async function getServerSideProps(ctx: NextPageContext) {
-  const { page } = ctx.query;
+  const { page, title, languages, tags } = ctx.query;
+  const header = [
+    { field: "id", header: "id" },
+    { field: "title", header: "제목" },
+    { field: "memory_limit", header: "메모리" },
+    { field: "time_limit", header: "소요 시간" },
+    { field: "languages", header: "언어" },
+  ];
+
   if (page) {
-    const result = await getProblems({ page: page as string });
-    const { page: pageInfo, problems } = result.data;
+    try {
+      const result = await getProblems({
+        page: page as string,
+        title,
+        languages,
+        tags,
+      });
 
-    const header = [
-      { field: "id", header: "id" },
-      { field: "title", header: "제목" },
-      { field: "memory_limit", header: "메모리" },
-      { field: "time_limit", header: "소요 시간" },
-      { field: "languages", header: "언어" },
-    ];
+      const { page: pageInfo, problems } = result.data;
 
-    return {
-      props: {
-        header,
-        problems,
-        pageInfo,
-      },
-    };
+      return {
+        props: {
+          header,
+          problems,
+          pageInfo,
+        },
+      };
+    } catch (e) {
+      return {
+        props: {
+          header,
+          problems: [],
+        },
+      };
+    }
   }
 }
 
