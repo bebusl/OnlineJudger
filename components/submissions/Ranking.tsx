87362b92a5e50f3109138f7bd0bcf3d98633141d
bebusl <bebus1998@naver.com>
2022-12-02 21:17:04 +0900

    feat: 랭킹 컴포넌트 구현
    * 랭킹 페이지에 랭킹 컴포넌트 삽입한 것 커밋을 까먹어서 rebase 함

--- components/submissions/Ranking.tsx
+++ components/submissions/Ranking.tsx
@@ -1,23 +1,20 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { getSubmissionsByQuery } from "../../api/submissionsAPI";
 import useInfiniteScroll from "../../hooks/useIntersectionObserver";
 import { Table } from "../common";
 import Modal from "../common/Modal";
 import ScrollBox from "../common/ScrollBox";
 
-function Ranking({
-  onClose,
-  problemId,
-}: {
-  onClose: Function;
-  problemId: number;
-}) {
+function Ranking({ problemId }: { problemId?: number }) {
+  const [body, setBody] = useState<
+    Record<string, string | number | JSX.Element>[]
+  >([]);
+  const [page, setPage] = useState(0);
+  const pageLimit = useRef<number>(1);
   const onIntersectionCallback = () => {
-    setPage((prev) => prev + 1);
+    if (page < pageLimit.current - 1) setPage((prev) => prev + 1);
   };
   const { targetRef } = useInfiniteScroll({ onIntersectionCallback });
-  const [body, setBody] = useState<Record<string, unknown>[]>([]);
-  const [page, setPage] = useState(0);
 
   const header = useMemo(
     () => [
@@ -36,18 +33,34 @@ function Ranking({
       const result = await getSubmissionsByQuery({
         problemId,
         isRanking: true,
+        page: page,
       });
       if (result.data?.success) {
-        setBody((prev) => [...prev, ...result.data.submissions]);
+        pageLimit.current = result.data.page.total_pages;
+        setBody((prev) => [...prev, ...result.data?.submissions]);
       }
     })();
   }, [page]);
 
   return (
+    <>
+      <Table header={header} body={body} />
+      <div style={{ height: "1px" }} ref={targetRef}></div>
+    </>
+  );
+}
+
+export function RankingModal({
+  onClose,
+  problemId,
+}: {
+  onClose: Function;
+  problemId: number;
+}) {
+  return (
     <Modal onClose={onClose} title="문제 풀이">
       <ScrollBox>
-        <Table header={header} body={body} />
-        <div style={{ height: "1px" }} ref={targetRef}></div>
+        <Ranking problemId={problemId} />
       </ScrollBox>
     </Modal>
   );
