80faf451323b5d0c0d62ad176fa21e2ca8d66157
bebusl <bebus1998@naver.com>
2022-12-10 22:08:14 +0900

    refactor: 서치필터 리팩토링

--- components/search/SearchFilter.tsx
+++ components/search/SearchFilter.tsx
@@ -1,70 +1,29 @@
 import { useRouter } from "next/router";
-import React, {
-  ChangeEventHandler,
-  FormEventHandler,
-  useEffect,
-  useState,
-} from "react";
+import React, { ChangeEventHandler, FormEventHandler, useRef } from "react";
 import styled from "styled-components";
 import { C, CPP, JAVA, PYTHON2, PYTHON3 } from "../../constants/language";
+import { TAGS } from "../../constants/tag";
+import { toggleSetItem } from "../../utils/setTypeUtils";
 import { FlexBox } from "../common";
 import SearchBar from "../common/SearchBar";
 import Selector from "../common/Selector";
 
-const static_tags = [
-  "입출력",
-  "사칙연산",
-  "조건문",
-  "반복문",
-  "함수",
-  "배열",
-  "문자열",
-  "브루트포스",
-  "이분 탐색",
-  "에라토스테네스의 체",
-  "스택",
-  "큐",
-  "덱",
-  "정렬",
-  "해싱",
-  "다이나믹 프로그래밍",
-  "그래프 탐색(DFS/BFS)",
-  "집합과 맵",
-  "우선순위 큐",
-  "분할 정복",
-  "좌표 압축",
-  "백트래킹",
-  "트리",
-  "최단거리알고리즘(데이크스트라등)",
-  "분리집합",
-  "누적합",
-  "배낭문제",
-  "위상정렬",
-  "최소스패닝트리",
-  "비트마스킹",
-  "LIS",
-  "LCS",
-];
-
 function SearchFilter() {
   const router = useRouter();
-  const [selectedLanguage, setSelectedLanguage] = useState(new Set());
-  const [selectedTag, setSelectedTag] = useState(new Set());
 
-  useEffect(() => {
-    const queries = router.query;
-    const { languages, tags } = queries;
-    if (languages) {
-      const newQuery = new Set(
-        Array.isArray(languages) ? languages : [languages]
-      );
-      setSelectedLanguage(newQuery);
+  function makeQuerySet(queries: string | string[] | undefined): Set<string> {
+    let newQuerySet = new Set<string>();
+    if (queries) {
+      newQuerySet = Array.isArray(queries)
+        ? new Set(queries)
+        : new Set([queries]);
     }
-    if (tags) {
-      const newQuery = new Set(Array.isArray(tags) ? tags : [tags]);
-      setSelectedTag(newQuery);
-    }
-  }, []);
+    return newQuerySet;
+  }
+
+  const { languages, tags } = router.query;
+  const languagesQuery = makeQuerySet(languages);
+  const tagsQuery = makeQuerySet(tags);
 
   const handleTitleSubmit: FormEventHandler = (e) => {
     e.preventDefault();
@@ -83,28 +42,20 @@ function SearchFilter() {
   const handleLanguageOptionChange: ChangeEventHandler<HTMLInputElement> = (
     e
   ) => {
-    const newQuery = new Set(selectedLanguage);
-    if (selectedLanguage.has(e.target.value)) newQuery.delete(e.target.value);
-    else newQuery.add(e.target.value);
+    const value = e.target.value as string;
+    const newLanguageQuery = toggleSetItem<string>(languagesQuery, value);
     router.push({
       pathname: router.pathname,
-      query: {
-        ...router.query,
-        languages: Array.from(newQuery) as string[],
-      },
+      query: { ...router.query, languages: Array.from(newLanguageQuery) },
     });
   };
 
   const handleTagChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    const newQuery = new Set(selectedTag);
-    if (selectedTag.has(e.target.value)) newQuery.delete(e.target.value);
-    else newQuery.add(e.target.value);
+    const value = e.target.value;
+    const newTagsQuery = toggleSetItem<string>(tagsQuery, value);
     router.push({
       pathname: router.pathname,
-      query: {
-        ...router.query,
-        tags: Array.from(newQuery) as string[],
-      },
+      query: { ...router.query, tags: Array.from(newTagsQuery) },
     });
   };
 
@@ -121,44 +72,80 @@ function SearchFilter() {
           options={[
             {
               text: C,
-              checked: selectedLanguage.has(C),
+              checked: languagesQuery.has(C),
             },
-            { text: CPP, checked: selectedLanguage.has(CPP) },
-            { text: JAVA, checked: selectedLanguage.has(JAVA) },
+            { text: CPP, checked: languagesQuery.has(CPP) },
+            { text: JAVA, checked: languagesQuery.has(JAVA) },
             {
               text: PYTHON2,
-              checked: selectedLanguage.has(PYTHON2),
+              checked: languagesQuery.has(PYTHON2),
             },
             {
               text: PYTHON3,
-              checked: selectedLanguage.has(PYTHON3),
+              checked: languagesQuery.has(PYTHON3),
             },
           ]}
           onChange={handleLanguageOptionChange}
           groupName="채점가능언어"
         />
         <Selector
-          options={static_tags.map((tag) => ({
+          options={TAGS.map((tag) => ({
             text: tag,
-            checked: selectedTag.has(tag),
+            checked: tagsQuery.has(tag),
           }))}
           onChange={handleTagChange}
           groupName="태그"
         />
       </FlexBox>
-      <div style={{ width: "100%", height: "2rem", margin: "1rem" }}>
-        {Array.from(selectedLanguage).map((a) => (
+      <div style={{ minWidth: "2rem" }}>
+        {Array.from(languagesQuery).map((a) => (
           <Tag key={a}>
             {a}
-            <button>x</button>
+            <button
+              onClick={() => {
+                languagesQuery.delete(a);
+                router.push({
+                  pathname: router.pathname,
+                  query: {
+                    ...router.query,
+                    languages: Array.from(languagesQuery),
+                  },
+                });
+              }}
+            >
+              x
+            </button>
           </Tag>
         ))}
-        {Array.from(selectedTag).map((t) => (
+        {Array.from(tagsQuery).map((t) => (
           <Tag key={t}>
             {t}
-            <button>x</button>
+            <button
+              onClick={() => {
+                tagsQuery.delete(t);
+                router.push({
+                  pathname: router.pathname,
+                  query: {
+                    ...router.query,
+                    tags: Array.from(tagsQuery),
+                  },
+                });
+              }}
+            >
+              x
+            </button>
           </Tag>
         ))}
+        {!!(tagsQuery.size || languagesQuery.size) && (
+          <Tag
+            as="button"
+            onClick={() => {
+              router.push({ pathname: router.pathname });
+            }}
+          >
+            초기화
+          </Tag>
+        )}
       </div>
     </>
   );
@@ -174,4 +161,9 @@ const Tag = styled.span`
   background-color: #f6d8d8;
   border-radius: 5px;
   font-size: ${({ theme }) => theme.fontSizes[0]};
+  & > button {
+    border: none;
+    background: none;
+    box-shadow: none;
+  }
 `;
