68e8114ac0a2929884276f9dc0d6cba17c7cefd7
bebusl <bebus1998@naver.com>
2022-12-10 22:37:00 +0900

    fix: Oauth 리다이렉트 링크 변경

--- constants/url.ts
+++ constants/url.ts
@@ -5,10 +5,10 @@ export const LINK_KEY = "linkKey";
 
 export const API_BASE_URL = "https://online-judge-api.yoonleeverse.com";
 export const WEB_SOCKET_URL = "https://online-judge-api.yoonleeverse.com/ws";
-export const OAUTH2_REGISTER_REDIRECT_URI =
-  "https://localhost:3443/oauth2/redirect";
-export const OAUTH2_LINK_REDIRECT_URI =
-  "https://localhost:3443/oauth2/redirect/link";
+
+const WEB_URL = "https://uni-online-judger.vercel.app/";
+export const OAUTH2_REGISTER_REDIRECT_URI = WEB_URL + "oauth2/redirect";
+export const OAUTH2_LINK_REDIRECT_URI = WEB_URL + "oauth2/redirect/link";
 
 export const GOOGLE_AUTH_REGISTER_URL =
   API_BASE_URL +
