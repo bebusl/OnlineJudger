0f4a3d618d030ce6aea702d1c9bb8208464b570f
bebusl <bebus1998@naver.com>
2022-12-10 22:13:56 +0900

    feat: 유틸 함수 생성

--- utils/authUtils.ts
+++ utils/authUtils.ts
@@ -3,5 +3,4 @@ import Cookies from "js-cookie";
 export const makeAuthHeader = () => {
   const auth_token = Cookies.get("Authorization") as string;
   if (auth_token) return { Authorization: auth_token };
-  else return null;
 };
--- utils/validator.ts
+++ utils/validator.ts
@@ -1,7 +1,8 @@
 export type regexType = "email" | "password" | "id" | string;
 
 const regexPatterns: Record<regexType, RegExp> = {
-  email: new RegExp("[a-zA-Z0-9]+([-_.][a-zA-Z0-9]*)*@[a-zA-Z]{3,}.[a-z]{2,4}"),
+  //email: new RegExp("[a-zA-Z0-9]+([-_.][a-zA-Z0-9]*)*@[a-zA-Z]{3,}.[a-z]{2,4}"),
+  email: new RegExp("[a-zA-Z0-9]{6,}"),
   password: new RegExp(
     "(^.*(?=^.{8,15}$)(?=.*d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$)|[a-zA-Z0-9]{6,10}"
   ),
