6e6e38e4596304e35101d2ab1a273802f4fb6059
bebusl <bebus1998@naver.com>
2022-12-12 22:58:52 +0900

    design: IconButton 스타일 수정

--- components/common/Buttons/IconButton/IconButton.tsx
+++ components/common/Buttons/IconButton/IconButton.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Button from "../BasicButton/Button";
 
 interface IconButtonProps {
-  iconSrc?: string;
+  iconSrc: string;
   text: string;
   subText: string;
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
@@ -17,24 +17,25 @@ export const IconButton = ({
   isActive,
 }: IconButtonProps) => (
   <Button
+    width="100%"
     style={
       isActive
         ? {
             display: "flex",
-            borderRadius: "15px",
             border: "1px solid #6358dc",
             backgroundColor: "#d5d9ff",
           }
         : {
             display: "flex",
-            borderRadius: "15px",
             border: "1px solid #ededed",
             backgroundColor: "#fff",
           }
     }
     onClick={onClick}
   >
-    <div style={{ flex: 1, padding: "5%" }}></div>
+    <div style={{ flex: 1 }}>
+      {iconSrc && <Image src={iconSrc} width="40px" height="40px" alt="icon" />}
+    </div>
     <div style={{ flex: 2, margin: "auto 0" }}>
       <h4 style={{ textAlign: "left", margin: 0, color: "#000" }}>{text}</h4>
       <p
