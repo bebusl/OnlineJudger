98e08a1b20fd653d3be32baa0d0182989c2f5cd5
이진희 <49019236+bebusl@users.noreply.github.com>
2022-12-15 12:56:29 +0900

    feat: OAuth 가입, 연동 페이지 SSR -> SR
    * SEO가 없는 페이지고, 사용자에게 노출되지도 않는 페이지여서 SSR 할 필요가 없음
    * 뒤로가기 버튼을 눌렀을 때 해당 페이지로 들어올 수 없도록 router.push -> router.replace로 변경
    * query의 타입 좁히기

--- pages/oauth2/redirect/link.tsx
+++ pages/oauth2/redirect/link.tsx
@@ -2,53 +2,30 @@ import React, { useEffect } from "react";
 import type { NextPageContext } from "next";
 
 import { useRouter } from "next/router";
-import { useAppDispatch } from "../../../hooks/useStore";
 import useNotification from "../../../hooks/useNotification";
 import { linkOauth } from "../../../api/authAPI";
 
-function Oauth2Link({
-  isNew,
-  linkKey,
-  error,
-  provider,
-}: {
-  isNew: string;
-  linkKey: string;
-  error: string;
-  provider: string;
-}) {
+function Oauth2Link() {
   const router = useRouter();
   const addNotification = useNotification();
+  const { newUser, linkKey } = router.query;
+
   useEffect(() => {
-    if (isNew) {
+    const isLinkable = newUser === "true" && typeof linkKey === "string";
+    if (isLinkable) {
       (async () => {
         const response = await linkOauth(encodeURI(linkKey));
         if (response.data.success) {
-          addNotification("연동에 성공했습니다.", "success");
-          router.replace("/user");
+          addNotification("연동에 성공했습니다", "success");
         }
       })();
     } else {
-      addNotification("다른 계정과 연동된 소셜 계정입니다.", "error");
+      addNotification("다른 계정과 연동된 소셜 계정입니다");
     }
+    router.replace("/user");
   }, []);
 
   return <div>연동하는 곳!</div>;
-}
-
-export function getServerSideProps(ctx: NextPageContext) {
-  const isNew = ctx.query?.newUser;
-  const linkKey = ctx.query?.linkKey ?? "";
-  const error = ctx.query?.error ?? "";
-  const provider = ctx.query?.provider;
-  return {
-    props: {
-      isNew: isNew === "true" ? true : false,
-      linkKey,
-      error,
-      provider,
-    },
-  };
 }
 
 export default Oauth2Link;
