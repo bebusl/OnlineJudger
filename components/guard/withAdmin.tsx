d016a8d8f6bd75a86358fee37f359bb6e8c7b371
이진희 <49019236+bebusl@users.noreply.github.com>
2022-12-18 04:00:28 +0900

    fix: Guard hoc 권한 체크 조건 수정
    * 인증 중이 아니고, 로그인도 안한 상태며 쿠키에 Authorization 토큰이 없으면 권한 없음 처리
    * 관리자의 경우 거기에 ROLE도 체크
    
    * 인증 중이라면 로딩컴포넌트가 보이도록 수정해야 함

--- components/guard/withAdmin.tsx
+++ components/guard/withAdmin.tsx
@@ -4,31 +4,39 @@ import { useAppSelector } from "../../hooks/useStore";
 import useNotification from "../../hooks/useNotification";
 
 import { ROLEADMIN } from "../../utils/constants/role";
+import { getAuthToken } from "../../utils/authUtils";
 
 const withAdmin = (WrappedComponent: React.ComponentType<Record<string, unknown>>) => {
   function AuthenticatedComponent(props: Record<string, unknown>) {
     const router = useRouter();
     const addNotification = useNotification();
-    const { isLogin, roles } = useAppSelector((state) => state.auth);
+    const { isLogin, roles, isAuthenticating } = useAppSelector((state) => state.auth);
     const [mounted, setMounted] = useState(false);
+
+    const authToken = getAuthToken();
 
     useEffect(() => {
       setMounted(true);
     }, []);
 
     if (mounted) {
-      if (!isLogin) {
-        addNotification("로그인이 필요합니다.", "error");
-        router.replace("/");
-        return null;
+      if (isAuthenticating) return <div>권한 확인중..</div>;
+      if (!isAuthenticating && !isLogin) {
+        addNotification("관리자 로그인이 필요합니다", "error");
+        router.back();
+      }
+      if (!isAuthenticating && isLogin && roles?.includes(ROLEADMIN)) {
+        return <WrappedComponent {...props} />;
       }
       // if (isLogin && roles?.includes(ROLEADMIN))
       //   return <WrappedComponent {...props} />;
-      return <WrappedComponent {...props} />;
+      addNotification("관리자 권한이 없습니다", "error");
+      router.push("/login");
       // addNotification("관리자 권한이 없습니다.", "error");
       // router.replace("/");
     }
     return null;
+    //여기 로딩을 넣어볼까!
   }
   AuthenticatedComponent.displayName = "authenticated-component";
   return AuthenticatedComponent;
