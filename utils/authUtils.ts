294a0ec1537ea78a0d5b0bbd3c6c61bc50ee0dd5
이진희 <49019236+bebusl@users.noreply.github.com>
2022-12-23 09:15:19 +0900

    feat: authUtil generateAuthHeader -> getAuthToken

--- utils/authUtils.ts
+++ utils/authUtils.ts
@@ -1,10 +1,9 @@
 import Cookies from "js-cookie";
 import { addHours } from "./dateUtils";
 
-export const generateAuthHeader = () => {
-  const auth_token = Cookies.get("Authorization") as string;
-  if (auth_token) return { Authorization: auth_token };
-  return null;
+export const getAuthToken = () => {
+  const authToken = Cookies.get("Authorization") as string;
+  return authToken;
 };
 
 export const setAuthorizationCookie = (accessToken: string) => {
@@ -16,8 +15,5 @@ export const setAuthorizationCookie = (accessToken: string) => {
 };
 
 export const removeAuthorizationCookie = () => {
-  Cookies.remove("Authorization", {
-    secure: false,
-    sameSite: "Strict",
-  });
+  Cookies.remove("Authorization");
 };
