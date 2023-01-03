c392b057f395ee323fdba8d6a8a57a17accda7cb
이진희 <49019236+bebusl@users.noreply.github.com>
2023-01-04 04:45:33 +0900

    feat: ScrollBox 스타일 수정
    * padding 추가
    * 예상한 것보다 박스 사이즈 커지지 않도록 box-sizing: border-box 추가

--- components/common/ScrollBox.tsx
+++ components/common/ScrollBox.tsx
@@ -3,8 +3,10 @@ import styled from "styled-components";
 const ScrollBox = styled.div<{ width?: string; height?: string }>`
   width: ${({ width }) => width ?? "100%"};
   position: relative;
+  box-sizing: border-box;
   top: 0;
   left: 0;
+  padding: 1rem;
   height: ${({ height }) => height ?? "100%"};
   overflow: scroll;
 `;
