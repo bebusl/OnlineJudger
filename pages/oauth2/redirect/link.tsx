0b10ba63bab8cfa2e3c6c4de8761ca8a20eb2fbe
이진희 <49019236+bebusl@users.noreply.github.com>
2023-01-06 08:37:18 +0900

    style: OAuth 연동 페이지 코드 스타일 수정

--- pages/oauth2/redirect/link.tsx
+++ pages/oauth2/redirect/link.tsx
@@ -17,12 +17,12 @@ function Oauth2Link() {
         const response = await linkOauth(encodeURI(linkKey));
         if (response.data.success) {
           addNotification("연동에 성공했습니다", "success");
+        } else {
+          addNotification("다른 계정과 연동된 소셜 계정입니다");
         }
+        router.replace("/user");
       })();
-    } else {
-      addNotification("다른 계정과 연동된 소셜 계정입니다");
     }
-    router.replace("/user");
   }, []);
 
   return <div>연동하는 곳!</div>;
